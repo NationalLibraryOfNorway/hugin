7031be41dbe3e74165ac290262c715e934864deb
Fredrik Monsen <fredrik.monsen@nb.no>
2024-05-06 15:24:38 +0200

    Add sentry

---
+++ src/app/global-error.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any */
+'use client';
+
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
+import {useEffect} from 'react';
+
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <Error statusCode={undefined as any}/>
+      </body>
+    </html>
+  );
+}
