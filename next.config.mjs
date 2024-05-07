4a1edafb7924f48deb82468393cd3b190770f608
Fredrik Monsen <fredrik.monsen@nb.no>
2024-05-07 10:03:43 +0200

    Add sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
+import {withSentryConfig} from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
     basePath: "/hugin",
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+    silent: true,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+    url: process.env.SENTRY_URL
+}, {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    hideSourceMaps: true,
+    disableLogger: true
+});
