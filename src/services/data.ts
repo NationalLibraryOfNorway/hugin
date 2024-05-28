6ebb621d21dc1c375ce6aad6a19e30dbab3477e1
Fredrik Monsen <fredrik.monsen@nb.no>
2024-05-28 14:18:48 +0200

    Fix pipeline

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,11 +1,14 @@
 import {Title} from '@/models/Title';
 
 export async function searchNewspaperTitles(searchTerm: string, signal: AbortSignal): Promise<Title[]> {
-  const response = await fetch(`/api/title/search?searchTerm=${searchTerm}&materialType=NEWSPAPER`, {signal});
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_PATH}/api/title/search?searchTerm=${searchTerm}&materialType=NEWSPAPER`,
+    {signal}
+  );
   return await response.json() as Promise<Title[]>;
 }
 
 export async function fetchNewspaperTitle(id: string): Promise<Title> {
-  const response = await fetch(`/api/title?catalogueId=${id}&materialType=NEWSPAPER`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/api/title?catalogueId=${id}&materialType=NEWSPAPER`);
   return await response.json() as Promise<Title>;
 }
