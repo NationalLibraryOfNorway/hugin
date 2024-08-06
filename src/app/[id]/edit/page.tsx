bbaac76a6f56427fffda6c5fb3b1b345768ad59b
Marius Levang <marius.levang@nb.no>
2024-08-06 13:27:56 +0200

    add titlestring to successmodal

--- src/app/[id]/edit/page.tsx
+++ src/app/[id]/edit/page.tsx
@@ -81,7 +81,7 @@ export default function Page({params}: { params: { id: string } }) {
         type='button'
         className='bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-fit mb-5'
         startContent={<FaArrowAltCircleLeft/>}
-        onClick={() => router.push(`/${params.id}`)}
+        onClick={() => router.push(`/${params.id}?title=${titleString}`)}
       >
         Tilbake til titteloversikt
       </Button>
@@ -325,7 +325,7 @@ export default function Page({params}: { params: { id: string } }) {
                 <SuccessModal
                   text='Lagret!'
                   buttonText='Til titteloversikt'
-                  buttonOnClick={() => router.push(`/${params.id}`)}
+                  buttonOnClick={() => router.push(`/${params.id}?title=${titleString}`)}
                 />}
           </div>
         </div>
