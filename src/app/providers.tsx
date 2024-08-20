469772e796bd4bcb3fc06039d4b6e6beed4c9cd4
Fredrik Monsen <fredrik.monsen@nb.no>
2024-08-20 10:50:17 +0200

    fix token refresh

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
+import {SessionProvider} from 'next-auth/react';
 import {NextUIProvider} from '@nextui-org/react';
+import {Session} from 'next-auth';
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({children, session}: { children: React.ReactNode; session?: Session | null }) {
   return (
   /* Refetch interval set to fetch 15 seconds before the token expires */
-    <SessionProvider basePath="/hugin/api/auth" refetchInterval={4.75 * 60}>
+    <SessionProvider session={session} basePath="/hugin/api/auth" refetchInterval={4.75 * 60}>
       <NextUIProvider>
         {children}
       </NextUIProvider>
