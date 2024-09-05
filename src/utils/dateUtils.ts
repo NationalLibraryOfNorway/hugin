757735b8ded54f45dd6275a98e849ac690f569b4
Marius Levang <marius.levang@nb.no>
2024-09-05 12:38:03 +0200

    3-change-box-reg-calendar, rebased

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,4 +1,12 @@
+import {CalendarDate} from '@nextui-org/react';
+import {parseDate} from '@internationalized/date';
+
 export function createCatalogDateString(date: Date | null | undefined): string {
   if (!date) return '';
   return new Date(date).toISOString().split('T')[0];
+}
+
+export function dateToCalendarDate(date: Date | null): CalendarDate {
+  const usedDate = date ? date : new Date();
+  return parseDate(new Date(usedDate).toISOString().split('T')[0]);
 }
