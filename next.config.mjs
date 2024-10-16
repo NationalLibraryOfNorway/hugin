643c3e1864054d478f81257966a7c400aacb4955
Fredrik Monsen <31658585+fredrikmonsen@users.noreply.github.com>
2024-10-16 12:16:48 +0200

    fix: exclude AuthProvider from StrictMode in local development to prevent auth problems (#57)
    Local dev without disabling strict mode for the whole app in the config
    caused auth problems as the code to exchange to token was sent twice,
    causing infinite loop of re-rendering. This PR disables strict mode in
    the config while adding it with <React.StrictMode> below <AuthProvider>
    in the tree.
    
    This PR should have no effect any environment except development

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import {withSentryConfig} from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
     output: "standalone",
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     experimental: {
