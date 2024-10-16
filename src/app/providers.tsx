61c3491a29e80605872d69c281092f67c0f52a21
Fredrik Monsen <fredrik.monsen@nb.no>
2024-10-16 10:51:09 +0200

    fix: exclude AuthProvider from StrictMode in local development to prevent auth problems

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,16 @@
 
 import {NextUIProvider} from '@nextui-org/react';
 import {AuthProvider} from '@/app/AuthProvider';
+import React from 'react';
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <NextUIProvider locale='nb-NO'>
-        {children}
-      </NextUIProvider>
+      <React.StrictMode>
+        <NextUIProvider locale='nb-NO'>
+          {children}
+        </NextUIProvider>
+      </React.StrictMode>
     </AuthProvider>
   );
 }
