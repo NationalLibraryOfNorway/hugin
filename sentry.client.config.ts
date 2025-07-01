cec4649a86b2f1709cd9387e81b21935868f4e84
Sindre Østrem <sindre.ostrem@nb.no>
2025-07-01 13:02:45 +0200

    feat: Add production deployment (TT-2012)

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,6 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.SERVER_DSN,
+  environment: process.env.NODE_ENV,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
